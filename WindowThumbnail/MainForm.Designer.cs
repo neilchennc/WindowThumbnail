82d784e625f2c6a7a5ddc25135187111269d706c
Neil Chen <neilchennc@gmail.com>
2021-05-15 13:25:38 +0800

    first commit

---
+++ WindowThumbnail/MainForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace WindowThumbnail
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WindowListControl = new System.Windows.Forms.ComboBox();
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.ThumbnailControl = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ThumbnailControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WindowListControl
+            // 
+            this.WindowListControl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.WindowListControl.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.WindowListControl.FormattingEnabled = true;
+            this.WindowListControl.Location = new System.Drawing.Point(1, 1);
+            this.WindowListControl.Name = "WindowListControl";
+            this.WindowListControl.Size = new System.Drawing.Size(243, 23);
+            this.WindowListControl.TabIndex = 0;
+            this.WindowListControl.SelectedIndexChanged += new System.EventHandler(this.WindowListControl_SelectedIndexChanged);
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RefreshButton.Location = new System.Drawing.Point(245, 1);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(75, 23);
+            this.RefreshButton.TabIndex = 1;
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // ThumbnailControl
+            // 
+            this.ThumbnailControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ThumbnailControl.Location = new System.Drawing.Point(0, 25);
+            this.ThumbnailControl.Name = "ThumbnailControl";
+            this.ThumbnailControl.Size = new System.Drawing.Size(320, 180);
+            this.ThumbnailControl.TabIndex = 2;
+            this.ThumbnailControl.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 205);
+            this.Controls.Add(this.ThumbnailControl);
+            this.Controls.Add(this.RefreshButton);
+            this.Controls.Add(this.WindowListControl);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(200, 150);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Window Thumbnail";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.ThumbnailControl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox WindowListControl;
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.PictureBox ThumbnailControl;
+    }
+}
+
---
+++ WindowThumbnail/MainForm.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WindowThumbnail
+{
+    public partial class MainForm : Form
+    {
+        #region Constants
+
+        static readonly int GWL_STYLE = -16;
+        static readonly int GWL_EXSTYLE = -20;
+
+        static readonly int DWM_TNP_VISIBLE = 0x8;
+        static readonly int DWM_TNP_OPACITY = 0x4;
+        static readonly int DWM_TNP_RECTDESTINATION = 0x1;
+
+        static readonly ulong WS_VISIBLE = 0x10000000L;
+        static readonly ulong WS_BORDER = 0x00800000L;
+        static readonly ulong WS_EX_TOOLWINDOW = 0x00000080L;
+        static readonly ulong TARGETWINDOW = WS_BORDER | WS_VISIBLE;
+
+        #endregion
+
+        #region DWM functions
+
+        [DllImport("dwmapi")]
+        static extern int DwmRegisterThumbnail(IntPtr dest, IntPtr src, out IntPtr thumb);
+
+        [DllImport("dwmapi")]
+        static extern int DwmUnregisterThumbnail(IntPtr thumb);
+
+        [DllImport("dwmapi")]
+        static extern int DwmQueryThumbnailSourceSize(IntPtr thumb, out PSIZE size);
+
+        [DllImport("dwmapi")]
+        static extern int DwmUpdateThumbnailProperties(IntPtr hThumb, ref DWM_THUMBNAIL_PROPERTIES props);
+
+        #endregion
+
+        #region Win32 helper functions
+
+        [DllImport("user32", CharSet = CharSet.Unicode)]
+        static extern ulong GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32")]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32")]
+        static extern int EnumWindows(EnumWindowsCallback lpEnumFunc, int lParam);
+        delegate bool EnumWindowsCallback(IntPtr hwnd, int lParam);
+
+        [DllImport("user32", CharSet = CharSet.Unicode)]
+        public static extern void GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        #endregion
+
+        private IntPtr Thumb;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            GetWindows();
+        }
+
+        #region Retrieve list of windows
+
+        private void GetWindows()
+        {
+            WindowListControl.Items.Clear();
+            EnumWindows(EnumWindowCallback, 0);
+        }
+
+        private bool EnumWindowCallback(IntPtr hwnd, int lParam)
+        {
+            //if (this.Handle != hwnd && (GetWindowLong(hwnd, GWL_STYLE) & TARGETWINDOW) == TARGETWINDOW)
+            if (this.Handle != hwnd && (GetWindowLong(hwnd, GWL_STYLE) & WS_VISIBLE) != 0 && (GetWindowLong(hwnd, GWL_EXSTYLE) & WS_EX_TOOLWINDOW) == 0)
+            {
+                StringBuilder sb = new StringBuilder(256);
+                GetWindowText(hwnd, sb, sb.Capacity);
+
+                WindowListControl.Items.Add(new Window
+                {
+                    Handle = hwnd,
+                    Title = sb.ToString()
+                });
+            }
+
+            return true; // continue enumeration
+        }
+
+        #endregion
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            if (Thumb != IntPtr.Zero)
+                DwmUnregisterThumbnail(Thumb);
+
+            GetWindows();
+        }
+
+        private void WindowListControl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Window w = (Window)WindowListControl.SelectedItem;
+
+            if (Thumb != IntPtr.Zero)
+                DwmUnregisterThumbnail(Thumb);
+
+            int i = DwmRegisterThumbnail(this.Handle, w.Handle, out Thumb);
+
+            if (i == 0)
+                UpdateThumb();
+        }
+
+        #region Update thumbnail properties
+
+        private void UpdateThumb()
+        {
+            if (Thumb != IntPtr.Zero)
+            {
+                DwmQueryThumbnailSourceSize(Thumb, out PSIZE size);
+
+                DWM_THUMBNAIL_PROPERTIES props = new DWM_THUMBNAIL_PROPERTIES
+                {
+                    fVisible = true,
+                    dwFlags = DWM_TNP_VISIBLE | DWM_TNP_RECTDESTINATION | DWM_TNP_OPACITY,
+                    //opacity = (byte)opacity.Value,
+                    opacity = 255,
+                    rcDestination = new Rect(ThumbnailControl.Left, ThumbnailControl.Top, ThumbnailControl.Right, ThumbnailControl.Bottom)
+                };
+
+                if (size.x < ThumbnailControl.Width)
+                    props.rcDestination.Right = props.rcDestination.Left + size.x;
+
+                if (size.y < ThumbnailControl.Height)
+                    props.rcDestination.Bottom = props.rcDestination.Top + size.y;
+
+                DwmUpdateThumbnailProperties(Thumb, ref props);
+            }
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            UpdateThumb();
+        }
+
+        #endregion
+    }
+
+    internal class Window
+    {
+        public string Title;
+        public IntPtr Handle;
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+
+    #region Interop structs
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct DWM_THUMBNAIL_PROPERTIES
+    {
+        public int dwFlags;
+        public Rect rcDestination;
+        public Rect rcSource;
+        public byte opacity;
+        public bool fVisible;
+        public bool fSourceClientAreaOnly;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct Rect
+    {
+        internal Rect(int left, int top, int right, int bottom)
+        {
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct PSIZE
+    {
+        public int x;
+        public int y;
+    }
+
+    #endregion
+}
